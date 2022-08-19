8506fd653f149c5992f0b77490a1bd101ae1e5d0
Carlo Tasca <c.tasca.1971@gmail.com>
2022-08-19 18:22:10 +0200

    Use docker SDK python package - Improve docker service module - Use docker SDK python package to determine php-fpm container name

--- docker/service.py
+++ docker/service.py
@@ -1,12 +1,21 @@
 import string
+import docker
+import re
 
 class Service:
-    def __init__(self, docker_dir:string, container: string) -> None:
+    def __init__(self, docker_dir : string) -> None:
         self.docker_dir = docker_dir
-        self.container = container
-    
-    def command(self, command):
+
+    def php_fpm(self) -> string:
+        client = docker.from_env()
+        containers = client.containers.list()
+        for container in containers:
+            if re.search("php-fpm", container.name):
+                return container.name
+        raise Exception("No php-fpm container found")
+
+    def command(self, container : string, command : string):
         """
         Runs command on a docker container
         """
-        return "cd {} & docker exec -it {} bash -c \"{}\"".format(self.docker_dir, self.container, command)
+        return "cd {} & docker exec -it {} bash -c \"{}\"".format(self.docker_dir, container, command)
