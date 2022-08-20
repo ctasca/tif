978f018b31e86700bdcc4386a0e9653c8952034a
Carlo Tasca <c.tasca.1971@gmail.com>
2022-08-20 14:44:02 +0200

    Implement docker containers options list - Add exec method to docker Service class

--- cli/options.py
+++ cli/options.py
@@ -3,18 +3,26 @@ import re
 import os
 from tif.fabric import cli
 from fabric.connection import Connection
+from tif.docker.service import Service
 
 class Options:
     def __init__(self, **options) -> None:
         self.options = options
 
-    def render(self, input_text= None) -> string:
+    def render(self, input_text= None, confirm_prompt = True) -> string:
         self._print()
         if (input_text):
             option = input(input_text)
         else:
             option = input("Choose an option: ")
-        return self._get_option(option.strip())
+        return self._get_option(option.strip(), confirm_prompt=confirm_prompt)
+
+    def docker_container_chooser(self, service : Service, confirm_prompt = True) -> string:
+        containers = service.containers_dictionary()
+        containers[str(len(containers) + 1)] = "Exit"
+        self.options = containers
+        return self.render(input_text="Choose a container: ", confirm_prompt=confirm_prompt)
+
 
     def remote_directory_chooser(self, c : Connection, dir = None, absolute_path = True, input_text = None, list_all = False):
         options = []
@@ -212,13 +220,18 @@ class Options:
         for key in self.options.keys():
             print (cli.puts_hide(">>> {}".format(key)), '--', self.options[key])
 
-    def _get_option(self, option):
+    def _get_option(self, option, confirm_prompt = True):
         if (option.strip() not in self.options.keys()):
                 cli.puts("!!! Not a valid choice")
+                exit()
         elif (self.options[option].lower() == 'exit'):
             exit()
         else:
-            choise = cli.cli_confirm("You have chosen '{}'. Continue?".format(self.options[option]))
-            if (choise == 'y'):
-                return self.options[option]
-            exit()
+            if confirm_prompt:
+                choise = cli.cli_confirm("You have chosen '{}'. Continue?".format(self.options[option]))
+                if (choise == 'y'):
+                    return self.options[option]
+                else:
+                    cli.puts("!!! Aborted")
+                    exit()
+            return self.options[option]
--- docker/service.py
+++ docker/service.py
@@ -1,6 +1,8 @@
 import string
 import docker
 import re
+from invoke import run
+from tif.fabric import cli
 
 class Service:
     def __init__(self, docker_dir : string) -> None:
@@ -13,6 +15,29 @@ class Service:
             if re.search(search, container.name):
                 return container.name
         raise Exception("No {} container match".format(search))
+
+    def containers_dictionary(self) -> dict:
+        """
+        Returns dictionary with numbers as key and container's name as value
+        """
+        containers_dict = {}
+        client = docker.from_env()
+        containers = client.containers.list()
+        index = 1
+        for container in containers:
+            containers_dict[str(index)] = container.name
+            index = index + 1
+        return containers_dict
+
+    def exec(self, options, command = None, confirm_prompt = True):
+        """
+        Exec a command in chosen container. Expect Options object from tif.cli.options module
+        """
+        container_choice = options.docker_container_chooser(self, confirm_prompt=confirm_prompt)
+        if not command:
+            command = cli.prompt(">>> Enter command to execute: ")
+        command = self.command(container_choice, command.strip())
+        run(command, pty=True)
 
     def command(self, container : string, command : string):
         """
